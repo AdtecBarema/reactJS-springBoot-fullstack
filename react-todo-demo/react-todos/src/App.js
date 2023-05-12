a6c388effda01d660f86b811eb2f35d0d3e8894c
addisu mekonnen marilign <addisumekonnenmarilign@Addisus-MBP.home>
2023-05-12 12:39:26 -0500

    react basics

--- react-todo-demo/react-todos/src/App.js
+++ react-todo-demo/react-todos/src/App.js
@@ -1,64 +1,56 @@
-import React, { useState } from "react";
-import "./App.css";
-import TodoTables from "./components/TodoTables";
-import NewTodoForm from "./components/NewTodoForm";
+import React, {useState} from 'react';
+import './App.css';
+import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
 
-  const [showTodoForm, setShowTodoForm] = useState(false);
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
 
   const [todos, setTodos] = useState([
-    { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "user one" },
-    { rowNumber: 2, rowDescription: "water plants", rowAssigned: "user two" },
-    { rowNumber: 3, rowDescription: "make dinner", rowAssigned: "user one" },
-    {
-      rowNumber: 4,
-      rowDescription: "Charge phone battery",
-      rowAssigned: "user one",
-    },
-    {
-      rowNumber: 5,
-      rowDescription: "Clean bath room",
-      rowAssigned: "user three",
-    },
-  ]);
+    {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'Hanna'},
+    {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'Abraham'},
+    {rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'Dady'},
+    {rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'Hanna'}
+  ]
+  )
 
-  const addToDo = (description, assigned) => {
+  const addTodo = (description, assigned) => {
     let rowNumber = 0;
-
     if (todos.length > 0) {
-      rowNumber = todos[todos.length - 1].rowNumber +1;
-    }
-    else{
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
       rowNumber = 1;
-
     }
       const newTodo = {
         rowNumber: rowNumber,
         rowDescription: description,
-        rowAssigned: assigned,
+        rowAssigned: assigned
       };
-      setTodos((todos) => [...todos, newTodo]);
-    }
+      setTodos(todos => [...todos, newTodo])
+  }
 
-    const deleteTodo =(deletetodoRowNumber)=>{
-      let filtered = todos.filter(function(value){
-        return value.rowNumber !== deletetodoRowNumber;
-      });
-      setTodos(filtered);
-    }
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered);
+  }
 
   return (
-    <div className="mt-5 container">
+    <div className='mt-5 container'>
       <div className="card">
-        <div className="card-header">Family Tasks and Your Todos</div>
+        <div className="card-header">
+          Your Todo's
+        </div>
         <div className="card-body">
-          <TodoTables todos={todos} deleteTodo ={deleteTodo} />
-          <button onClick={()=>setShowTodoForm(!showTodoForm)} className="btn btn-primary" >
-         { showTodoForm ? 'Close New Todo': 'New Todo'}
+          <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+          <button onClick={() => setShowAddTodoForm(!showAddTodoForm)} className='btn btn-primary'>
+            {showAddTodoForm ? 'Close New Todo' : 'New Todo'}
           </button>
-          {showTodoForm && <NewTodoForm addToDo ={addToDo}/> }
-          
+        {showAddTodoForm && 
+          <NewTodoForm addTodo={addTodo}/>
+        }
         </div>
       </div>
     </div>
